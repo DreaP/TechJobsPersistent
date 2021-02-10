3ebf6cd08f150645e3479a5a0007e3e4c909401c
Pruitt <apruitt89@outlook.com>
2021-02-09 21:00:36 -0600

    Added new job view model

---
+++ TechJobsPersistent/ViewModels/AddJobViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechJobsPersistent.ViewModels
+{
+    public class AddJobViewModel
+    {
+        public int EmployerId { get; set; }
+        public string JobName { get; set; }
+        public List<SelectListItem> Skills { get; set; }
+    }
+}
