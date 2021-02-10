431381854ec0ed0f3c32b7e6bfd3b66dfce33444
Pruitt <apruitt89@outlook.com>
2021-02-09 22:15:08 -0600

    Changes to controllers, employerviewmodel and start up.

---
+++ TechJobsPersistent/ViewModels/AddEmployerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechJobsPersistent.ViewModels
+{
+    public class AddEmployerViewModel
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Location can have only 1 location!")]
+        public string Location { get; set; }
+    }
+}
